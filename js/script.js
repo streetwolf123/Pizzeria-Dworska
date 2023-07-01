77828d0c1368c0483ce5f60c8711913ac5f7e1ea
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-07-01 21:35:03 +0200

    function for time added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let toggle = true;
+
+
+
+if (toggle = true) {
+    let timeSlot = document.querySelector(".time__paragraph");
+
+
+    setInterval(() => {
+        const newDate = new Date();
+        const h = newDate.getHours();
+        const m = newDate.getMinutes();
+        const s = newDate.getSeconds();
+        
+        timeSlot.innerHTML = h + ":" + m + ":" + s;
+    }, 1000)
+}
+
 
 
 
